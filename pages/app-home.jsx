d21a8e2efa3215827e5f6b11af6d750a997a50da
Avi Iskaov <avitech1242@gmail.com>
2022-04-27 11:15:58 +0300

    avis first commit

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -1,8 +1,9 @@
 const { Link } = ReactRouterDOM
 
-export function AppHome(){
-
-  return <section className="app-home-container">
-    <h1>Hello from App Home!</h1>
-  </section>
+export function AppHome() {
+  return (
+    <section className="app-home-container">
+      <h1>Hello from App Home 2!</h1>
+    </section>
+  )
 }
