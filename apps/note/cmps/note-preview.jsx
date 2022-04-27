67cea8f194677305145a5c5f6b3d7ad72a9981a0
Avi Iskaov <avitech1242@gmail.com>
2022-04-27 15:09:04 +0300

    adding img and video notes

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -3,15 +3,15 @@ export function NotePreview({ note, onDeleteNote }) {
   return (
     <div className="note">
       <div className="note-content">
-        <span>{info.txt}</span>
+        <span>{getNoteContent(info)}</span>
       </div>
       <div className="note-footer">
-        {/* <span className="created-at">{note.createdAt}</span> */}
-        <div className="tools-container fa-lg">
-          <i className="fa fa-thumb-tack fa-lg"></i>
-          <i className="fa fa-pencil-square-o fa-lg"></i>
-          <i className="fa fa-envelope fa-lg"></i>
-          <i className="fa fa-paint-brush fa-lg"></i>
+        <span className="created-at">{note.createdAt}</span>
+        <div className="tools-container fa-md">
+          <i className="fa fa-thumb-tack fa-md"></i>
+          <i className="fa fa-pencil-square-o fa-md"></i>
+          <i className="fa fa-envelope fa-md"></i>
+          <i className="fa fa-paint-brush fa-md"></i>
           <i
             onClick={() => {
               onDeleteNote(id)
@@ -22,4 +22,19 @@ export function NotePreview({ note, onDeleteNote }) {
       </div>
     </div>
   )
+}
+
+function getNoteContent(info) {
+  if (info.txt) return info.txt
+  if (info.imgUrl) return <img src={info.imgUrl}></img>
+  if (info.videoUrl)
+    return (
+      <iframe
+        src={`https://www.youtube.com/embed/${getVideoId(info.videoUrl)}`}
+      ></iframe>
+    )
+}
+
+function getVideoId(videoUrl) {
+  return videoUrl.substring(videoUrl.indexOf('=') + 1)
 }
